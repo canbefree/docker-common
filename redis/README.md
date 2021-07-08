49d6057865f927b63ca0300b867ae7644e76b433
neo <452198757@qq.com>
2021-07-08 11:34:20 +0800

    redis集群搭建~

--- redis/README.md
+++ redis/README.md
@@ -2,3 +2,6 @@
 ## cluster-tutorial
 
 https://redis.io/topics/cluster-tutorial
+
+### 创建集群脚本
+https://github.com/redis/redis/blob/unstable/utils/create-cluster/create-cluster
---
+++ redis/cluster/create-cluster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## example:
+#  create-cluster redis-7000:7000 redis-7001:7001 redis-7002:7002 
+#
+#
+
+if [ $# -lt 1 ]; then
+	echo "$0 need paramter"
+fi
+
+function getDomainIp() {
+	if test $1 == ""; then
+		echo "need a paramter"
+	fi
+	url=$(echo $1 | cut -d ":" -f1)
+	port=$(echo $1 | cut -d ":" -f2)
+	if test $port == $url; then
+		port="6379"
+	fi
+	result=$(ping $url -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
+}
+
+cmd="redis-cli --cluster create --cluster-yes --cluster-replicas "
+for domain in $@; do
+	getDomainIp $domain
+	cmd=$(printf "%s %s:%s " "$cmd" "$result" "$port")
+done
+
+$cmd
